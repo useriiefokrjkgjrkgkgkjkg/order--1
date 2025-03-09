1696e4cca98a9159f2129826044135765fd7f22a
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-09 14:37:53 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: 'black', height: '100vh' }}>
+      {/* Черный экран */}
+    </div>
+  );
+}
+
+export default App; 
