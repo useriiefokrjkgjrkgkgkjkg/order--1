b3c1a3ac6f71abbd504b0d5f8a588ebe024fd222
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-09 14:09:17 +0300

    Initial commit: Basic Next.js setup with black screen

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{
+      backgroundColor: 'black',
+      minHeight: '100vh',
+      width: '100%',
+      margin: 0,
+      padding: 0
+    }}>
+    </main>
+  );
+} 
